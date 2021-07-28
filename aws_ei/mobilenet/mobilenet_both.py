ae1b4961f51e9a424130b1832b5cc6804cf3cb62
Subin <39542978+subeans@users.noreply.github.com>
2021-07-28 17:13:06 +0900

    parmas load_model

--- aws_ei/mobilenet/mobilenet_both.py
+++ aws_ei/mobilenet/mobilenet_both.py
@@ -26,8 +26,10 @@ _ = tf.keras.applications.mobilenet.preprocess_input(temp)
 results = None
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchsize', default=8, type=int)
+parser.add_argument('--load_model',default=False , type=bool)
 args = parser.parse_args()
 batch_size = args.batchsize
+load_model = args.load_model
 # batch_size = 8
 
 ei_client = boto3.client('elastic-inference')
@@ -38,8 +40,6 @@ def load_save_model(saved_model_dir = 'mobilenet_saved_model'):
     shutil.rmtree(saved_model_dir, ignore_errors=True)
     model.save(saved_model_dir, include_optimizer=False, save_format='tf')
 
-saved_model_dir = 'mobilenet_saved_model' 
-load_save_model(saved_model_dir)
 
 def deserialize_image_record(record):
     feature_map = {'image/encoded': tf.io.FixedLenFeature([], tf.string, ''),
@@ -93,7 +93,10 @@ def get_dataset(batch_size, use_cache=False):
         dataset = dataset.cache(f'./tfdatacache/imagenet_val')
     
     return dataset
-
+    
+saved_model_dir = 'mobilenet_saved_model' 
+if load_model : 
+    load_save_model(saved_model_dir)
 
 print('\n=======================================================')
 print(f'Benchmark results for CPU Keras, batch size: {batch_size}')
