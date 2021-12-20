c7511c6ad10ecb0f690f0836c3255282f1ea6791
Subin <39542978+subeans@users.noreply.github.com>
2021-12-20 16:47:20 +0900

    inference using vm

--- tvm/inference_tensorflow_model.py
+++ tvm/inference_tensorflow_model.py
@@ -1,21 +1,26 @@
 import tensorflow as tf
 from tvm import relay
-from PIL import Image
-import os
 import numpy as np 
 import tvm 
-from tvm.contrib.download import download_testdata
-from tvm.contrib import graph_runtime
 from tvm.contrib import graph_executor
+import tvm.relay.testing.tf as tf_testing
 
+import tvm.testing 
+from tvm.runtime.vm import VirtualMachine
+import time
+import argparse
 
-batch_size = 1
+parser = argparse.ArgumentParser()
+parser.add_argument('--model',default='resnet50' , type=str)
+args = parser.parse_args()
+model_name = args.model
+# model_name = "resnet50"
+batch_size = 16
 size=224
-data,image_shape = make_dataset(batch_size,size)
-shape_dict = {"DecodeJpeg/contents": data.shape}
 
 def make_dataset(batch_size,size):
-    image_shape = (3, size, size)
+    image_shape = (size, size,3)
+    # image_shape = (3,size, size)
     data_shape = (batch_size,) + image_shape
 
     data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
@@ -29,50 +34,104 @@ def wrap_frozen_graph(graph_def, inputs, outputs, print_graph=False):
     wrapped_import = tf.compat.v1.wrap_function(_imports_graph_def, [])
     import_graph = wrapped_import.graph
 
-    print("-" * 50)
-    print("Frozen model layers: ")
-    layers = [op.name for op in import_graph.get_operations()]
-    if print_graph == True:
-        for layer in layers:
-            print(layer)
-    print("-" * 50)
+    # print("-" * 50)
+    # print("Frozen model layers: ")
+    # layers = [op.name for op in import_graph.get_operations()]
+    # if print_graph == True:
+    #     for layer in layers:
+    #         print(layer)
+    # print("-" * 50)
 
     return wrapped_import.prune(
         tf.nest.map_structure(import_graph.as_graph_element, inputs),
         tf.nest.map_structure(import_graph.as_graph_element, outputs))
 
-  
-with tf.io.gfile.GFile("./frozen_models/frozen_graph.pb", "rb") as f:
+
+# saved model -> frozen model 변환한 모델 호출하여 wrap 하기 
+with tf.io.gfile.GFile(f"./frozen_models/frozen_{model_name}.pb", "rb") as f:
         graph_def = tf.compat.v1.GraphDef()
-        loaded = graph_def.ParseFromString(f.read())
+        graph_def.ParseFromString(f.read())
+        graph = tf.import_graph_def(graph_def, name="")
+        # Call the utility to import the graph definition into default graph.
+        graph_def = tf_testing.ProcessGraphDefParam(graph_def)
+        try : 
+            with tf.compat.v1.Session() as sess:
+                graph_def = tf_testing.AddShapesToGraphDef(sess, "softmax")
+        except:
+            pass
 
 frozen_func = wrap_frozen_graph(graph_def=graph_def,
                                     inputs=["input_1:0"],
                                     outputs=["Identity:0"],
                                     print_graph=True)
 
+# test dataset 생성 
+data,image_shape = make_dataset(batch_size,size)
 
+print(data.shape)
+shape_dict = {"DecodeJpeg/contents": data.shape}
+
+##### Convert tensorflow model 
 mod, params = relay.frontend.from_tensorflow(graph_def, layout=None, shape=shape_dict)
 print("Tensorflow protobuf imported to relay frontend.")
+
 
 target = "llvm"
 ctx = tvm.cpu()
 
+##### TVM compile style 1 
+# print("-"*50)
+# print("Compile style 1 : vm compile ")
+# print("-"*50)
+# with tvm.transform.PassContext(opt_level=3):
+#     exe = relay.vm.compile(mod,target=target, params=params)
+
+# vm = VirtualMachine(exe,ctx)
+# _out = vm.invoke("main",data)
+# # print(_out)
+
+# input_data = tvm.nd.array(data)
+# input_list = [input_data]
+# warm_iterations=10
+# measurements=5
+
+# for i in range(warm_iterations):    # warm up
+#     vm.run(input_list)
+
+# start_time = time.time()
+# for i in range(measurements):
+#     vm.run(input_list)
+# end_time = time.time()
+# tvm_time = end_time - start_time
+# print("VM runtime time elapsed", tvm_time/measurements)
+# print("\n")
+
+##### TVM compile style 
+print("-"*50)
+print("Compile style : create_executor vm ")
+print("-"*50)
 with tvm.transform.PassContext(opt_level=3):
-    lib = relay.build(mod,target=target, params=params)
+    executor = relay.build_module.create_executor("vm", mod, tvm.cpu(0), target)
 
-# graph create 
-module = graph_executor.GraphModule(lib["default"](ctx))
-module.set_input("data", data)
-module.run()
-out_deploy = module.get_output(0).numpy()
+# tasks = autotvm.task.extract_from_program(
+#     mod["main"], target=target, params=params)
+# )
 
-lib.export_library(f"./resnet50_{batch_size}_{target}_deploy_lib.tar")
+executor.evaluate()(data,**params)
 
-data_tvm = tvm.nd.array(data.astype('float32'))
-e = module.module.time_evaluator("run", ctx, number=5, repeat=1)
-t = e(data_tvm).results
-t = np.array(t) * 1000
+input_data = tvm.nd.array(data)
+input_list = [input_data]
+warm_iterations=10
+measurements=5
 
-print('{} (batch={}): {} ms'.format('resnet50', batch_size, t.mean()))
+input_list = [input_data]
+for i in range(warm_iterations):    # warm up
+    executor.evaluate()(input_data, **params)
 
+start_time = time.time()
+for i in range(measurements):
+    executor.evaluate()(input_data, **params)
+end_time = time.time()
+tvm_time = end_time - start_time
+print(f"VM {model_name} runtime time elapsed", tvm_time/measurements,"s")
+print("\n")
